1f560027204b8ce22a9d763db33dce6ba7a1d0a4
sattaw8 <38538081+sattaw8@users.noreply.github.com>
2021-10-06 13:36:28 +0700

    rider view

--- pages/rider/view.tsx
+++ pages/rider/view.tsx
@@ -110,27 +110,198 @@ export default function view({}: Props): ReactElement {
 										</Row>
 										</Card>
 										<Card>
-										<Row>
-										<Col className="gutter-row" span={6}>
-											<span>ใบอนุญาตขับรถ</span>
-										</Col>
-										<Col className="gutter-row" span={6}>
-											<Field
-													label={{ text: "SsoID" }}
-													name="keyword"
-													type="text"
-													component={Button}
-													className="form-control round"
-													id="keyword"
-													placeholder="ชื่อ-สกุล, เบอร์โทรศัพท์"
-													isRange={true}
-												/>
-											</Col>
+											<Row gutter={10}>
+												<Col span={4}>
+													บัตรประชาชน
+												</Col>
+												<Col>
+												{/* <Form.Item label="Field A" required tooltip="This is a required field"> */}
+													<Button>ดูรูปภาพ</Button>
+												{/* </Form.Item> */}
+												</Col>
+											</Row>
+											<Row gutter={10}>
+												<Col span={4}>
+													บุคคลที่ติดต่อได้ในกรณีฉุกเฉิน
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "ชื่อ-สกุลของบุคคลที่ติดต่อได้ในกรณีฉุกเฉิน" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ชื่อ-สกุล"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "ความสัมพันธ์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ความสัมพันธ์"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "เบอร์โทรศัพท์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="เบอร์โทรศัพท์"
+														isRange={true}
+													/>
+												</Col>
+											</Row>
+											<Row>
+												<Col className="gutter-row" span={18} offset={4}>
+													<Field
+														label={{ text: "ที่อยู่" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ที่อยู่"
+														isRange={true}
+													/>
+												</Col>
+											</Row>
+											<Row gutter={10}>
+												<Col span={4}>
+													บุคคลอ้างอิง
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "ชื่อ-สกุลของบุคคลอ้างอิง" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ชื่อ-สกุล"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "ความสัมพันธ์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ความสัมพันธ์"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "เบอร์โทรศัพท์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="เบอร์โทรศัพท์"
+														isRange={true}
+													/>
+												</Col>
+											</Row>
+											<Row>
+												<Col className="gutter-row" span={18} offset={4}>
+													<Field
+														label={{ text: "ที่อยู่" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ที่อยู่"
+														isRange={true}
+													/>
+												</Col>
+											</Row>
+											<Row gutter={10}>
+												<Col span={4}>
+													รายละเอียดรถ
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "กรรมสิทธิ์เจ้าของจักรยานยนต์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="กรรมสิทธิ์เจ้าของจักรยานยนต์"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "ยี่ห้อ/รุ่นรถจักรยานยนต์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ยี่ห้อ/รุ่นรถจักรยานยนต์"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "เลขทะเบียนรถจักรยานยนต์" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="เลขทะเบียนรถจักรยานยนต์"
+														isRange={true}
+													/>
+												</Col>
+											</Row>
+											<Row gutter={10}>
+												<Col className="gutter-row" span={6} offset={4}>
+													<Button>ดูรูปภาพ</Button>
+												</Col>
+												<Col className="gutter-row" span={6}>
+													<Button>ดูรูปภาพ</Button>
+												</Col>
+											</Row>
+											<Row gutter={10}>
+												<Col span={4}>
+													ความบกพร่องทางร่างกาย
+												</Col>
+												<Col className="gutter-row" span={6}>
+												<Field
+														label={{ text: "ความบกพร่องทางร่างกาย" }}
+														name="keyword"
+														type="text"
+														component={Input}
+														className="form-control round"
+														id="keyword"
+														placeholder="ความบกพร่องทางร่างกาย"
+														isRange={true}
+													/>
+												</Col>
+												<Col className="gutter-row" span={6}>
+													<Button>ดูรูปภาพ</Button>
+												</Col>
 											</Row>
 										</Card>
 									</Form>
 								)}
-								
 							</Formik>
             hello
         </MainLayout>

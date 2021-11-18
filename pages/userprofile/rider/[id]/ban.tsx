f5228dd1943bc29a96dfa5e8faee3168ac4b6f3e
Pikanate Makewan <pikanate.mak@fullteam.tech>
2021-11-18 11:30:10 +0700

    change directory rider profile

---
+++ pages/userprofile/rider/[id]/ban.tsx
@@ -0,0 +1,37 @@
+import Card from "@/components/Card";
+import MainLayout from "@/layout/MainLayout";
+import type { NextPage } from "next";
+
+const Ban: NextPage = () => {
+  return (
+    <MainLayout>
+      <Card>
+        Ban Rider Page
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </Card>
+    </MainLayout>
+  );
+};
+
+export default Ban;

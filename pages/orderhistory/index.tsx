c23e7098fbd5e1cb8992d91adfd90b1171c38d1e
Peerawich Chanloha <peerawich.cha@fullteam.tech>
2022-05-03 21:46:41 +0700

    fix default starttime, endtime

--- pages/orderhistory/index.tsx
+++ pages/orderhistory/index.tsx
@@ -45,8 +45,8 @@ const initialValues = {
   endtime: moment().endOf('day').format('HH:mm:ss'),
   order_number: '',
   client_time: {
-    start: moment().startOf('day').format('YYYY-MM-DD'),
-    end: moment().endOf('day').format('YYYY-MM-DD'),
+    start: moment().startOf('day').format('YYYY-MM-DD HH:mm:ss'),
+    end: moment().endOf('day').format('YYYY-MM-DD HH:mm:ss'),
   },
   sso_id: null,
   order_overall_status: null,

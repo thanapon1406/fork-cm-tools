bdaac58e575d6e6fec391094fd9993c061e47488
Anukoon Jiwboon <anukoon.jiw@fullteam.tech>
2022-05-06 14:41:26 +0700

    [ADD] Ordercheck

---
+++ pages/api/order/find-orders.ts
@@ -0,0 +1,19 @@
+import { API_PATH_ORDER_SERVICE, PREFIX_ORDER } from '@/constants/api'
+import ServerFetch from '@/services/api'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method, headers, body } = req
+  if (method === 'POST') {
+    try {
+      const url: string = `${API_PATH_ORDER_SERVICE}${PREFIX_ORDER}/find-orders`
+      const { data, status } = await ServerFetch.get(url, headers, body)
+      res.status(status).json(data)
+    } catch (e: any) {
+      res.status(e.response?.status || 500).json(e.response?.data)
+    }
+  } else {
+    res.setHeader('Allow', ['POST'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}

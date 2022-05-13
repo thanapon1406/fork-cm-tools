2dd61d30c9b389f747ccf6857fb42f7818bef828
Peerawich Chanloha <peerawich.cha@fullteam.tech>
2022-05-13 11:21:34 +0700

    modal_popup - app_id number

--- pages/banner/modal-pop-up/index.tsx
+++ pages/banner/modal-pop-up/index.tsx
@@ -8,6 +8,7 @@ import MainLayout from '@/layout/MainLayout'
 import { getModalPopUpList } from '@/services/modalPopUp'
 import { Breadcrumb, Col, Row, Typography } from 'antd'
 import { Field, Form, Formik } from 'formik'
+import { toNumber } from 'lodash'
 import moment from 'moment'
 import { useRouter } from 'next/router'
 import React, { ReactElement, useEffect, useState } from 'react'
@@ -72,12 +73,12 @@ export default function Merchant({ }: Props): ReactElement {
 
   const fetchDataActive = async (filterObj: filterObject = filter, paging: Pagination = paginationActive) => {
     const reqBody = {
+      ...filterObj,
+      app_id: toNumber(filterObj.app_id),
       is_eligible: true,
       page: paging.current,
       per_page: paging.pageSize,
-      ...filterObj,
     }
-    console.log(`reqBody Active`, reqBody)
     setIsLoading(true)
     const { result, success } = await getModalPopUpList(reqBody)
     if (success) {
@@ -95,12 +96,12 @@ export default function Merchant({ }: Props): ReactElement {
 
   const fetchDataInactive = async (filterObj: filterObject = filter, paging: Pagination = paginationActive) => {
     const reqBody = {
+      ...filterObj,
+      app_id: toNumber(filterObj.app_id),
       is_eligible: false,
       page: paging.current,
       per_page: paging.pageSize,
-      ...filterObj,
     }
-    console.log(`reqBody Inactive`, reqBody)
     setIsLoadingInactive(true)
     const { result, success } = await getModalPopUpList(reqBody)
     if (success) {
@@ -118,7 +119,6 @@ export default function Merchant({ }: Props): ReactElement {
 
 
   const handleSubmit = (values: any) => {
-    console.log(`values`, values)
     let reqFilter: filterObject = {
       keyword: values.keyword,
       status: values.status,

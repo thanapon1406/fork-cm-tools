ff6c14e85473884c54740de5c2312e09723fb093
sattaw8 <38538081+sattaw8@users.noreply.github.com>
2022-06-20 13:57:09 +0700

    fix bug1

--- pages/config-delivery-fee/create.tsx
+++ pages/config-delivery-fee/create.tsx
@@ -438,7 +438,8 @@ export default function ConfigDeliveryCreate({ }: Props): ReactElement {
           duration: 3,
         })
       }
-      if (_.get(result, "validate[0]") || _.get(result, "tier_duplicate_location")) {
+
+      if (_.get(result, "validate[0]") && _.get(result, "tier_duplicate_location")) {
         setShowError(true)
         result.validate.forEach((element: any, index: number) => {
           let districtData: any = _.find(locations, function (obj) {
@@ -451,7 +452,7 @@ export default function ConfigDeliveryCreate({ }: Props): ReactElement {
           let sub_districtDatas: any = []
 
           if (districtData.location_type !== "district") {
-            result.validate[index].sub_district.forEach((subdistrictId: any) => {
+            _.get(result, "result.validate[index].sub_district", []).forEach((subdistrictId: any) => {
               let sub_districtData: any = _.find(districtData.sub_districts, function (obj) {
                 if (obj.id == subdistrictId) {
                   return true;
@@ -465,6 +466,7 @@ export default function ConfigDeliveryCreate({ }: Props): ReactElement {
         const reqCreateTierPriceDelete: any = {
           id: responseTierPrice.result.tier_id,
         }
+        console.log("testdelete", reqCreateTierPriceDelete);
         await tierPriceDelete(reqCreateTierPriceDelete)
         setShowErrorResult(result.validate)
       }
